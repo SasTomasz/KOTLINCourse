8f279387bf97275d793931bfabf8f6be861886ca
SasTomasz <boss7@op.pl>
2019-08-22 05:28:28 +0200

    program works

--- app/src/main/java/com/example/android/kotlincourse/AMS.kt
+++ app/src/main/java/com/example/android/kotlincourse/AMS.kt
@@ -2,18 +2,25 @@ package com.example.android.kotlincourse
 
 import java.util.*
 
-fun main(args: Array<String>){
-    println("Your fortune is: " + getFortuneCookies())
+fun main(args: Array<String>) {
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortuneCookie()
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break
+    }
 }
 
-fun getFortuneCookies(): String {
-    val fortune = listOf("You will have a great day!", "Things will go well for you today.",
-        "Enjoy a wonderful day of success.", "Be humble and all will turn out well.",
-        "Today is a good day for exercising restraint.", "Take it easy and enjoy life!",
-        "Treasure your friends because they are your greatest fortune.")
-    print("Enter your birthday: ")
-    var birthday = readLine()?.toIntOrNull() ?:1
-    birthday = birthday.div(7)
-    return fortune[birthday]
-
+fun getFortuneCookie() : String {
+    val fortunes = listOf( "You will have a great day!",
+        "Things will go well for you today.",
+        "Enjoy a wonderful day of success.",
+        "Be humble and all will turn out well.",
+        "Today is a good day for exercising restraint.",
+        "Take it easy and enjoy life!",
+        "Treasure your friends, because they are your greatest fortune.")
+    print("\nEnter your birthday: ")
+    val birthday = readLine()?.toIntOrNull() ?: 1
+    return fortunes[birthday.rem(fortunes.size)]
 }
+
