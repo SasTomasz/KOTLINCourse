f00465cd9d2107523c0696c4ffe25fa37904af02
SasTomasz <boss7@op.pl>
2019-08-22 05:51:11 +0200

    change for loop to while loop

--- app/src/main/java/com/example/android/kotlincourse/AMS.kt
+++ app/src/main/java/com/example/android/kotlincourse/AMS.kt
@@ -3,22 +3,25 @@ package com.example.android.kotlincourse
 import java.util.*
 
 fun main(args: Array<String>) {
-    var fortune: String
-    for (i in 1..10) {
+    var fortune = "initialize"
+    var index = 0
+    while (index < 10 && !fortune.contains("Take it easy")) {
         fortune = getFortuneCookie()
         println("\nYour fortune is: $fortune")
-        if (fortune.contains("Take it easy")) break
+        index++
     }
 }
 
-fun getFortuneCookie() : String {
-    val fortunes = listOf( "You will have a great day!",
+fun getFortuneCookie(): String {
+    val fortunes = listOf(
+        "You will have a great day!",
         "Things will go well for you today.",
         "Enjoy a wonderful day of success.",
         "Be humble and all will turn out well.",
         "Today is a good day for exercising restraint.",
         "Take it easy and enjoy life!",
-        "Treasure your friends, because they are your greatest fortune.")
+        "Treasure your friends, because they are your greatest fortune."
+    )
     print("\nEnter your birthday: ")
     val birthday = readLine()?.toIntOrNull() ?: 1
     return fortunes[birthday.rem(fortunes.size)]
