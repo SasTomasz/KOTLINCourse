4b078c54cacd781a5e65bea077790e71bc937c68
SasTomasz <boss7@op.pl>
2019-08-23 05:47:00 +0200

    working lambdas

--- app/src/main/java/com/example/android/kotlincourse/AMS.kt
+++ app/src/main/java/com/example/android/kotlincourse/AMS.kt
@@ -1,34 +1,18 @@
 package com.example.android.kotlincourse
 
-fun main(args: Array<String>){
-    whatShouldIDoToday(getMood())
-    whatShouldIDoToday(getMood(), temperature = 30)
-    whatShouldIDoToday(getMood())
-    whatShouldIDoToday(getMood(), weather = "windy")
-    whatShouldIDoToday(getMood(), weather = "calm", temperature = 19)
+import java.lang.Math.random
+import kotlin.random.Random
+
+fun main(){
+    println("How many size your dice have?: ")
+    val sidesOdDice = readLine()!!.toInt()
+    val rollDice = {sides: Int -> if (sides > 0) Random.nextInt(sides) else 0}
+    val rollDice2: (Int) -> Int = {size -> if (size > 0) Random.nextInt(size) else 0}
+    gamePlay(sidesOdDice, rollDice2)
+
 }
 
-fun whatShouldIDoToday (mood : String, weather : String = "Sunny", temperature : Int = 24) {
-    when{
-        isHappyMood(mood) && isSunnyWeather(weather) -> println("go for a walk")
-        isSadMood(mood) && isSunnyWeather(weather) && isWarm(temperature) -> println("go for a jogging")
-        isHappyMood(mood) || isSadMood(mood) && isWindyWeather(weather) -> println ("go for the gym")
-        isCold(temperature) && isCalmWeather(weather) -> println("go to swimming pool")
-        else -> println("Stay home and read.")
-    }
-}
-
-fun isHappyMood(mood: String) = mood == "happy"
-fun isSadMood(mood: String) = mood == "sad"
-fun isSunnyWeather(weather: String) = weather == "Sunny"
-fun isWindyWeather(weather: String) = weather == "windy"
-fun isCalmWeather(weather: String) = weather == "calm"
-fun isWarm(temperature: Int) = temperature >= 20
-fun isCold(temperature: Int) = temperature < 20
-fun getMood(): String{
-    val mood: String
-    print("What is Your mood: ")
-    mood = readLine()!!
-    return mood
+fun gamePlay(sides: Int, roll: (Int) -> Int){
+    print(roll(sides).toString())
 }
 
